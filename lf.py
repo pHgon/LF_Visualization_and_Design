8a4e6e03dcfaf5d86b7b497b35cb3d67b1afa54c
vwpinho <vwdpinho@inf.ufpel.edu.br>
2019-09-27 15:08:36 -0300

    Apenas movimentacao entre as vistas

---
+++ lf.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'lf.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1920, 1080)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(350, 200, 625, 434))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("../../Imagens/Captura de tela de 2019-09-13 12-29-12.png"))
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1920, 20))
+        self.menubar.setObjectName("menubar")
+        self.menuFIle = QtWidgets.QMenu(self.menubar)
+        self.menuFIle.setObjectName("menuFIle")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.toolBar = QtWidgets.QToolBar(MainWindow)
+        self.toolBar.setObjectName("toolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        self.actionOpen = QtWidgets.QAction(MainWindow)
+        self.actionOpen.setObjectName("actionOpen")
+        self.menuFIle.addAction(self.actionOpen)
+        self.menubar.addAction(self.menuFIle.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menuFIle.setTitle(_translate("MainWindow", "FIle"))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
+        self.actionOpen.setText(_translate("MainWindow", "Open"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
---
+++ main.py
@@ -0,0 +1,60 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication
+import sys
+from lf import Ui_MainWindow 
+
+class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    
+    def __init__(self, parent=None):
+        super(ExampleApp, self).__init__(parent)
+        self.angulo_horizontal = 7
+        self.angulo_vertical = 7
+        self.setupUi(self)
+        self.openppm()
+
+    def get_vert_ang(self):
+        return self.angulo_vertical  
+
+    def get_horz_ang(self):
+        return self.angulo_horizontal
+
+    def openppm(self):
+        self.label.setPixmap(QtGui.QPixmap("/home/vwdpinho/Documentos/Bikes/Bikes/00" + str(self.get_vert_ang()) + "_00" + str(self.get_horz_ang()) + ".ppm"))
+
+    def keyPressEvent(self,event):
+        if event.key() == QtCore.Qt.Key_Left:
+            if(self.angulo_horizontal > 0):
+                self.angulo_horizontal -= 1
+            self.label.setPixmap(QtGui.QPixmap("/home/vwdpinho/Documentos/Bikes/Bikes/" + ("{0:0>3}".format(str(self.angulo_vertical))) + "_" + ("{0:0>3}".format(str(self.angulo_horizontal))) + ".ppm"))
+
+            
+
+        if event.key() == QtCore.Qt.Key_Right:
+            if(self.angulo_horizontal < 14):
+                self.angulo_horizontal += 1
+            self.label.setPixmap(QtGui.QPixmap("/home/vwdpinho/Documentos/Bikes/Bikes/" + ("{0:0>3}".format(str(self.angulo_vertical))) + "_" + ("{0:0>3}".format(str(self.angulo_horizontal))) + ".ppm"))
+           
+
+        if event.key() == QtCore.Qt.Key_Up:
+            if(self.angulo_vertical > 0):
+                self.angulo_vertical -= 1
+            self.label.setPixmap(QtGui.QPixmap("/home/vwdpinho/Documentos/Bikes/Bikes/" + ("{0:0>3}".format(str(self.angulo_vertical))) + "_" + ("{0:0>3}".format(str(self.angulo_horizontal))) + ".ppm"))
+            
+            
+
+        if event.key() == QtCore.Qt.Key_Down:
+            if(self.angulo_vertical  < 14):
+                self.angulo_vertical += 1
+            self.label.setPixmap(QtGui.QPixmap("/home/vwdpinho/Documentos/Bikes/Bikes/" + ("{0:0>3}".format(str(self.angulo_vertical))) + "_" + ("{0:0>3}".format(str(self.angulo_horizontal))) + ".ppm"))
+            
+    
+
+def main():
+    app = QApplication(sys.argv)
+    form = ExampleApp()
+    form.show()
+    app.exec_()
+    #form.open_ppm()
+
+if __name__ == '__main__':
+    main()
