46d8dc153f8cdb868cc2bf60a1b0cb6dbeb9ac80
thiagobubolz <tlabubolz@inf.ufpel.edu.br>
2019-11-05 17:58:15 -0300

    update na função upsampling

--- upsampling.py
+++ upsampling.py
@@ -1,24 +1,66 @@
 from PIL import Image
-
 import numpy as np
+
+def getSaiUpLeft(path):
+    img = Image.open(path + "_tempSAIs/0.png")
+    np_im = np.array(img)
+    return np_im
+
+def getSaiUp(path):
+    img = Image.open(path + "_tempSAIs/1.png")
+    np_im = np.array(img)
+    return np_im
+
+def getSaiUpRight(path):
+    img = Image.open(path + "_tempSAIs/2.png")
+    np_im = np.array(img)
+    return np_im
+
+def getSaiLeft(path):
+    img = Image.open(path + "_tempSAIs/3.png")
+    np_im = np.array(img)
+    return np_im
+
+def getSaiRight(path):
+    img = Image.open(path + "_tempSAIs/5.png")
+    np_im = np.array(img)
+    return np_im
+
+def getSaiDownLeft(path):
+    img = Image.open(path + "_tempSAIs/6.png")
+    np_im = np.array(img)
+    return np_im
+
+def getSaiDown(path):
+    img = Image.open(path + "_tempSAIs/7.png")
+    np_im = np.array(img)
+    return np_im
+
+def getSaiDownRight(path):
+    img = Image.open(path + "_tempSAIs/8.png")
+    np_im = np.array(img)
+    return np_im
+
 
 def upsampling(path, times):
 
-    path = '/home/paulo/Git-Repository/LF_VizualizationApp/007_007.ppm'
+    img = Image.open(path + "_tempSAIs/4.png")
+    np_im = np.array(img)
 
-    img = Image.open(path)
-    img.save("teste.png")
+    shape = np_im.shape
 
-    #img2 = np.array(img)
-    #print (img2.shape)
-    return
+    new_image = Image.new('RGB', (shape[1], shape[0]), color = (0,0,0))
 
-    img = np.uint32(img)
-    cv2.imshow('SAI', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    new_image.save('test.png')
+    new_image_np = np.array(new_image)
+
+    print(new_image_np.shape)
+
+    for i in range(new_image_np[0,:]):
+        for j in range(new_image_np[1,:]):
+            for color in range(new_image_np[2,:]):
+                print(new_image_np[i][j][color])
+            
 
 
-
-
-upsampling("/home/thiago/Mestrado_cadeiras/Lytro_fake/Bikes/007_007.ppm", 2)
+upsampling('/home/thiago/Mestrado_cadeiras/LF_VizualizationApp/', 2)
