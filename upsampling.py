88b5ee0120c7972ffcd5a33e8ee6a58865beabab
thiagobubolz <tlabubolz@inf.ufpel.edu.br>
2019-11-08 13:27:29 -0300

    Upsampling funcional finalizado

--- upsampling.py
+++ upsampling.py
@@ -53,16 +53,23 @@ def upsampling(path, times):
 
     new_image = Image.new('RGB', (shape[1]*times, shape[0]*times), color = (0,0,0))
 
-    new_image.save('test.png')
     new_image_np = np.array(new_image)
 
-    print(new_image_np.shape)
+    Sais = [getSaiUpLeft(path), getSaiUp(path), getSaiUpRight(path), getSaiLeft(path), getSaiRight(path), getSaiDownLeft(path), getSaiDown(path), getSaiDownRight(path)]
 
     for color in range(new_image_np.shape[2]):
         for i in range(0,new_image_np.shape[0],2):
             for j in range(0,new_image_np.shape[1],2):
-                #FirstPixel
-                new_image_np[i][j][color] = mean(getSaiUpLeft(path)[i/2][j/2][color], getSaiUp(path)[i/2][j/2][color], getSaiLeft[i/2][j/2][color], np_im[i/2][j/2][color])
-            
+                #First Pixel
+                new_image_np[i][j][color] = (int((Sais[0])[i//2][j//2][color]) + int((Sais[1])[i//2][j//2][color]) + int((Sais[3])[i//2][j//2][color]) + int(np_im[i//2][j//2][color]))//times
+                #Second Pixel
+                new_image_np[i+1][j][color] = (int((Sais[1])[i//2][j//2][color]) + int((Sais[2])[i//2][j//2][color]) + int((Sais[4])[i//2][j//2][color]) + int(np_im[i//2][j//2][color]))//times
+                #Tird Pixel
+                new_image_np[i][j+1][color] = (int((Sais[3])[i//2][j//2][color]) + int((Sais[5])[i//2][j//2][color]) + int((Sais[6])[i//2][j//2][color]) + int(np_im[i//2][j//2][color]))//times
+                #Fourth Pixel
+                new_image_np[i+1][j+1][color] = (int((Sais[4])[i//2][j//2][color]) + int((Sais[6])[i//2][j//2][color]) + int((Sais[7])[i//2][j//2][color]) + int(np_im[i//2][j//2][color]))//times
+    
+    upsampled_image = Image.fromarray(new_image_np, 'RGB')
+    upsampled_image.save('my.png')
 
 upsampling('/home/thiago/Mestrado_cadeiras/LF_VizualizationApp/', 2)
